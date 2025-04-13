fafb78f93898c68287b0febdfdd37c1e7cf184ca
Muhammad Adnan <ak187429@gmail.com>
2025-04-13 16:35:18 +0500

    PreSelection Added & Selected Color updated to Green

--- example/lib/glb_loader_example.dart
+++ example/lib/glb_loader_example.dart
@@ -26,6 +26,7 @@ class GlbLoaderExampleState
                 modelPath: 'assets/models/Tooth-3.glb',
                 iblPath: 'assets/models/giuseppe_bridge_4k_ibl.ktx',
                 skyboxPath: 'assets/models/giuseppe_bridge_4k_skybox.ktx',
+                preselectedEntities: ["Teeth_Lower_1", "Teeth_Lower_2", "Teeth_Lower_3", "Neck"],
                 onSelectionChanged: (selectedEntities) {
                   setState(() {
                     _selectedEntities = selectedEntities;
